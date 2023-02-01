9a7cc283ff3b09b6c56c740b4bbc2d48d81320a1
Kevin Harrington <mad.hephaestus@gmail.com>
2023-02-01 15:46:14 -0500

    adding cleat

---
+++ cleat.groovy
@@ -0,0 +1,20 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Wedge
+
+double depth = 25
+
+
+if (args==null)
+	args=[100,10]
+double inset = args[1]
+double length = args[0]
+CSG bracket=new Cube(depth+inset,length,depth+inset).toCSG()
+				.toZMin()
+				.toYMin()
+				.toXMin()
+				.movex(-inset)
+CSG cleatWedge =bracket.difference( new Wedge(depth,length,depth).toCSG())
+
+					
+return [cleatWedge]
