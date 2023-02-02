8a1b133831854708f7bb2c432a69c24f8093fee5
Kevin Harrington <mad.hephaestus@gmail.com>
2023-02-02 11:54:01 -0500

    front board

--- ForkyLiftCad.groovy
+++ ForkyLiftCad.groovy
@@ -23,29 +23,36 @@ return new ICadGenerator(){
 			double calculatedTotalWidth = spacing*grid;
 			double braceInsetDistance=40
 			double boardThickness=6.3
-			double rodToBoardDistance =20
+			double rodToBoardDistance =15
 			double sideBraceDistacne =20
-			
+			double boxClearence = 8
+			double frontCutoutDistance = 20
 			CSG moveDHValues(CSG incoming,DHLink dh ){
 				TransformNR step = new TransformNR(dh.DhStep(0)).inverse()
 				Transform move = com.neuronrobotics.bowlerstudio.physics.TransformFactory.nrToCSG(step)
 				return incoming.transformed(move)
 			}
 			@Override
-			public ArrayList<CSG> generateCad(DHParameterKinematics arg0, int arg1) {
+			public ArrayList<CSG> generateCad(DHParameterKinematics kin, int linkIndex) {
 				// TODO Auto-generated method stub
 				ArrayList<CSG> back =[]
-				double height =arg0.getMaxEngineeringUnits(arg1)-arg0.getMinEngineeringUnits(arg1)
+				double height =kin.getMaxEngineeringUnits(linkIndex)-kin.getMinEngineeringUnits(linkIndex)
 				double bracing = rodlen - height - rodEmbedlen
 				def vitamins =[]
-				double boardWidth = calculatedTotalWidth*2-(braceInsetDistance*arg1)+sideBraceDistacne*2
+				double boardWidth = calculatedTotalWidth*2-(braceInsetDistance*linkIndex)+sideBraceDistacne*2
 				CSG board = new Cube(boardThickness,boardWidth,rodlen+sideBraceDistacne).toCSG()
 							.toZMin()
+				CSG cutout = new Cube(boardThickness,boardWidth-frontCutoutDistance*2,rodlen+sideBraceDistacne-frontCutoutDistance*2).toCSG()
+							.toZMin()
+							.movez(frontCutoutDistance)
 				CSG backBoard = board
 									.toXMax()
 									.movex(-rodToBoardDistance)
+				CSG frontBoard = board.difference(cutout)
+									.toXMin()
+									.movex(rodToBoardDistance)
 				for(double i=-calculatedTotalWidth;i<calculatedTotalWidth+1;i+=(calculatedTotalWidth*2)) {
-					def braceInsetDistanceArg1 = braceInsetDistance*arg1
+					def braceInsetDistanceArg1 = braceInsetDistance*linkIndex
 					if(i<0)
 						braceInsetDistanceArg1*=-1
 					CSG rod = new Cylinder(rodDiam/2, rodlen).toCSG()
@@ -53,26 +60,32 @@ return new ICadGenerator(){
 					CSG upperBearing = moveDHValues(vitamin_linearBallBearing_LM10UU
 							.toZMax()
 							.movez(bracing)
-							,arg0.getDhLink(arg1))
-					CSG lowerBearing = moveDHValues(vitamin_linearBallBearing_LM10UU.movez(rodEmbedlen),arg0.getDhLink(arg1))
+							,kin.getDhLink(linkIndex))
+					CSG lowerBearing = moveDHValues(vitamin_linearBallBearing_LM10UU.movez(rodEmbedlen),kin.getDhLink(linkIndex))
 					CSG MyBearing = lowerBearing
 							.union(upperBearing)
 							.movey(i-(braceInsetDistanceArg1))
-					MyBearing.setManipulator(arg0.getLinkObjectManipulator(arg1))
+					MyBearing.setManipulator(kin.getLinkObjectManipulator(linkIndex))
 					back.add(MyBearing)
 					back.add(rod)
 					vitamins.addAll([MyBearing,rod])
-					if(arg1==0)
-						rod.setManipulator(arg0.getRootListener())
+					if(linkIndex==0)
+						rod.setManipulator(kin.getRootListener())
 					else
-						rod.setManipulator(arg0.getLinkObjectManipulator(arg1-1))
+						rod.setManipulator(kin.getLinkObjectManipulator(linkIndex-1))
+				}
+				if(linkIndex!=2) {
+					kin.setDH_R(linkIndex, rodToBoardDistance*2+boardThickness*2+boxClearence)
 				}
 				
-				if(arg1==0)
-					backBoard.setManipulator(arg0.getRootListener())
-				else
-					backBoard.setManipulator(arg0.getLinkObjectManipulator(arg1-1))
-				back.add(backBoard)
+				if(linkIndex==0) {
+					backBoard.setManipulator(kin.getRootListener())
+					frontBoard.setManipulator(kin.getRootListener())
+				}else {
+					backBoard.setManipulator(kin.getLinkObjectManipulator(linkIndex-1))
+					frontBoard.setManipulator(kin.getLinkObjectManipulator(linkIndex-1))
+				}
+				back.addAll([backBoard,frontBoard])
 				
 				for(CSG c:vitamins) {
 					c.setColor(Color.SILVER)
